ed419b415ad706f5b121331b3f71ab870381e5dc
brianhabana <brianhabana@gmail.com>
2022-01-13 01:28:07 -0800

    added contract migration

--- core/migrations/2_deploy_contracts.js
+++ core/migrations/2_deploy_contracts.js
@@ -1,4 +1,6 @@
 const Factory = artifacts.require("UniswapV2Factory.sol");
+const Token1 = artifacts.require("Token1.sol");
+const Token2 = artifacts.require("Token2.sol");
 
 module.exports =  async function (deployer, network, addresses) {
   await deployer.deploy(Factory, addresses[0]); //send transaction to deployment  
@@ -9,7 +11,17 @@ module.exports =  async function (deployer, network, addresses) {
   if(network == 'mainnet') {
       token1Address = '';
       token2Address = '';
+  } else {
+    //deploy tokens
+    await deployer.deploy(Token1);
+    await deployer.deploy(Token2);
+    //get reference to tokens
+    const token1 = await Token1.deployed();
+    const token2 = await Token2.deployed();
+    //save addresses
+    token1Address = token1.address;
+    token2Address = token2.address;
   }
-  await factory.createPair();
+  await factory.createPair(token1Address, token2Address);
 
 };
